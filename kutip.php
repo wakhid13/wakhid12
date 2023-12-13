5323ab4268b8cff5d59e30443c64d1147420c10a
wakhid13 <wakhidahsanulqoshi@gmail.com>
2023-12-13 16:43:15 +0700

    first commit

---
+++ kutip.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = "Denafa";
+
+echo "Hello $name, Selamat belajar PHP" . PHP_EOL;
+
+echo "=================================" . PHP_EOL;
+
+echo 'Hello $name, Selamat belajar PHP' . PHP_EOL;
+
